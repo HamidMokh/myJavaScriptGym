7b62c00db7395f9a779eab7e3c2f687f9c6dea16
Hamid Mokhtari <mokhtarihamid44@gmail.com>
2022-06-27 05:49:37 +0100

    promises excercice 3

--- exercisePromises.js
+++ exercisePromises.js
@@ -13,16 +13,16 @@ promise.then(console.log)
 // or
 promise.then(resp => console.log(resp))
 // #3) Read about Promise.resolve() and Promise.reject(). How can you make
+// the above promise shorter with Promise.resolve() and console loggin "success"
 const promis = Promise.resolve(
   setTimeout(() => {
     console.log("success");
   }, 4000)
 );
-// the above promise shorter with Promise.resolve() and console loggin "success"
-
 
 // #4) Catch this error and console log 'Ooops something went wrong'
 Promise.reject('failed')
+
 
 // #5) Use Promise.all to fetch all of these people from Star Wars (SWAPI) at the same time.
 // Console.log the output and make sure it has a catch block as well.
